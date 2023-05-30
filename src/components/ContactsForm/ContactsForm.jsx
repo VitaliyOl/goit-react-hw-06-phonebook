d67947d1aca3db1a1d8c6b561c4dd1a63fb315b2
VitaliyOl <oliferchuk@gmail.com>
2023-05-30 19:42:08 +0300

    upd

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
 import { Form, FormLabel, FormInput, FormButton } from './ContactsForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { createContact } from 'redux/contactSlice';
 import { getContact } from 'redux/selectors';
 
 export const ContactsForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
   const contacts = useSelector(getContact);
 
   const dispatch = useDispatch();
@@ -18,22 +14,16 @@ export const ContactsForm = () => {
       : dispatch(createContact({ name, number }));
   };
 
-  const handleChange = e => {
-    const { name, value } = e.currentTarget;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'number') {
-      setNumber(value);
-    }
-  };
-
   const handleSubmit = e => {
-    e.preventDefault();
+    const { elements } = e.target;
+
+    const name = elements.name.value;
+    const number = elements.number.value;
 
     handleAddContact({ name, number });
 
-    setName('');
-    setNumber('');
+    elements.name.value = '';
+    elements.number.value = '';
   };
 
   return (
@@ -46,8 +36,6 @@ export const ContactsForm = () => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={handleChange}
-          value={name}
         />
       </FormLabel>
       <FormLabel>
@@ -58,8 +46,6 @@ export const ContactsForm = () => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={handleChange}
-          value={number}
         />
       </FormLabel>
 
